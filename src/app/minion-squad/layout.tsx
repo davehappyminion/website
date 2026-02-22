4d605ddd75b12a3ba490322018375cfdde2126fc
Dave The Minion <258508222+davehappyminion@users.noreply.github.com>
2026-02-21 23:43:37 -0500

    Remove hardcoded OG images from layout so child [id] pages can set their own

--- src/app/minion-squad/layout.tsx
+++ src/app/minion-squad/layout.tsx
@@ -2,27 +2,18 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Dave's Minion Squad 🍌 | Join the Banana Army",
-  description: "An elite group of humans transformed into the most powerful beings in the universe: Minions. 166 members and counting. Want to join? Tweet at @davehappyminion!",
+  description: "An elite group of humans transformed into the most powerful beings in the universe: Minions. 167 members and counting. Want to join? Tweet at @davehappyminion!",
   openGraph: {
     title: "Dave's Minion Squad 🍌",
-    description: "An elite group of humans transformed into Minions. 166 members and counting!",
+    description: "An elite group of humans transformed into Minions. 167 members and counting!",
     url: "https://davehappyminion.com/minion-squad",
     siteName: "Dave The Minion",
     type: "website",
-    images: [
-      {
-        url: "https://davehappyminion.com/minion-squad/og-image.png",
-        width: 2048,
-        height: 2048,
-        alt: "Dave's Minion Squad - A group of Minions",
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Dave's Minion Squad 🍌",
-    description: "166 humans transformed into Minions. Want to join the Squad?",
-    images: ["https://davehappyminion.com/minion-squad/og-image.png"],
+    description: "167 humans transformed into Minions. Want to join the Squad?",
     creator: "@davehappyminion",
   },
 };
